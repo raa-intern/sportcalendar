7d6c78557fcfd5fac948ddc34d703877050c18d8
vasyldzyuba <vasyl.dzuba1@gmail.com>
2021-10-26 15:38:49 +0300

    some fixes

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 import {
   FacebookLoginButton,
   GoogleLoginButton,
   TwitterLoginButton,
-} from 'react-social-login-buttons';
-
+} from "react-social-login-buttons";
+import TextField from "@mui/material/TextField";
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '80%',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 export default function LoginModal(props) {
+  const handleClose = props.handleClose;
+  const open = props.open;
   return (
     <div>
       <Modal
-        open={props.open}
-        onClose={props.handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
         <FacebookLoginButton />
         <GoogleLoginButton />
@@ -36,20 +38,20 @@ export default function LoginModal(props) {
           <Box
             sx={{
               width: 500,
-              maxWidth: '100%',
+              maxWidth: "100%",
             }}
           >
-            <TextField fullWidth label='Email' />
+            <TextField fullWidth label="Email" />
           </Box>
           <Box
             sx={{
               width: 500,
-              maxWidth: '100%',
+              maxWidth: "100%",
             }}
           >
-            <TextField fullWidth label='Password' />
+            <TextField fullWidth label="Password" />
           </Box>
-          <Button variant='contained'>Logi In</Button>
+          <Button variant="contained">Logi In</Button>
         </Box>
       </Modal>
     </div>
--- src/components/SecondHeader.jsx
+++ src/components/SecondHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   AppBar,
   Stack,
@@ -6,13 +6,13 @@ import {
   Link,
   ToggleButton,
   ToggleButtonGroup,
-} from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TelegramIcon from '@mui/icons-material/Telegram';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LoginModal from './LoginModal';
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LoginModal from "./LoginModal";
 
 export default function SecondHeader() {
   const [open, setOpen] = React.useState(false);
@@ -21,48 +21,48 @@ export default function SecondHeader() {
 
   return (
     <AppBar
-      position='static'
-      sx={{ flexDirection: 'row', justifyContent: 'space-around' }}
+      position="static"
+      sx={{ flexDirection: "row", justifyContent: "space-around" }}
     >
-      <Stack direction='row'>
-        <IconButton aria-label='facebook'>
+      <Stack direction="row">
+        <IconButton aria-label="facebook">
           <FacebookIcon />
         </IconButton>
-        <IconButton aria-label='telegram'>
+        <IconButton aria-label="telegram">
           <TelegramIcon />
         </IconButton>
-        <IconButton aria-label='instagram'>
+        <IconButton aria-label="instagram">
           <InstagramIcon />
         </IconButton>
       </Stack>
 
       <Link
-        href='tel:+380970000000'
-        underline='none'
-        color='success'
-        sx={{ display: 'flex', alignItems: 'center', mr: 2 }}
+        href="tel:+380970000000"
+        underline="none"
+        color="success"
+        sx={{ display: "flex", alignItems: "center", mr: 2 }}
       >
         <PhoneIphoneIcon />
         +380970000000
       </Link>
 
       <ToggleButtonGroup
-        size='small'
-        color='success'
-        value={'ukr'}
+        size="small"
+        color="success"
+        value={"ukr"}
         exclusive
         onChange={() => {}}
       >
-        <ToggleButton value='ukr'>УКР</ToggleButton>
-        <ToggleButton value='eng'>ENG</ToggleButton>
-        <ToggleButton value='rus'>РУС</ToggleButton>
+        <ToggleButton value="ukr">УКР</ToggleButton>
+        <ToggleButton value="eng">ENG</ToggleButton>
+        <ToggleButton value="rus">РУС</ToggleButton>
       </ToggleButtonGroup>
 
       <Link
-        href='#'
-        underline='none'
-        color='success'
-        sx={{ display: 'flex', alignItems: 'center', mr: 2 }}
+        href="#"
+        underline="none"
+        color="success"
+        sx={{ display: "flex", alignItems: "center", mr: 2 }}
         onClick={handleOpen}
       >
         <FavoriteIcon />
