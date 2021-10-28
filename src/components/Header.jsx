1ff39422ebcc4e04a0c46641aeb17df2ae8d8aff
ttareshchenko <ttareshchenko@rightandabove.com>
2021-10-28 16:27:27 +0300

    Add nav linls

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,49 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import SecondHeader from "./SecondHeader";
 import LoginModal from "./LoginModal";
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  Button,
-  IconButton,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Toolbar, Button, Typography } from "@mui/material";
+import { createStyles, makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    navLink: {
+      textDecoration: "none",
+      color: theme.palette.primary.contrastText,
+      "&:hover, &:focus": { color: theme.palette.secondary.light },
+    },
+  })
+);
 
 export default function ButtonAppBar() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const classes = useStyles();
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <SecondHeader />
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" component="div" sx={{ ml: 2 }}>
             Sport Calendar
           </Typography>
+          <NavLink to="/events" className={classes.navLink}>
+            Події
+          </NavLink>
+          <NavLink to="/organizers" className={classes.navLink}>
+            Організаторам
+          </NavLink>
+          <NavLink to="/cooperation" className={classes.navLink}>
+            Співпраця
+          </NavLink>
+          <NavLink to="/about" className={classes.navLink}>
+            Про нас
+          </NavLink>
           <LoginModal handleClose={handleClose} open={open} />
           <Button color="inherit" onClick={handleOpen}>
-            Login
+            Вхід | Реєстрація
           </Button>
         </Toolbar>
       </AppBar>
