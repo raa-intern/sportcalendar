6f22624b239baff617dca6ab8352913f98eff5c8
falconcloud2021 <83278758+falconcloud2021@users.noreply.github.com>
2021-11-23 08:49:27 +0200

    api.php
    upload api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/expenses', 'ExpenseController@index')->name('expenses.all');
+
+Route::post('/expenses', 'ExpenseController@store')->name('expenses.store');
+
+Route::get('/expenses/{expense}', 'ExpenseController@show')->name('expenses.show');
+
+Route::put('/expenses/{expense}', 'ExpenseController@update')->name('expenses.update');
+
+Route::delete('/expenses/{expense}', 'ExpenseController@destroy')->name('expenses.destroy');
